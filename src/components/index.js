325adedca14b67cc4bdbdffa7df489975c1720d2
Aliakbar <aliakbarkhanalipor0090@gmail.com>
2025-02-07 23:46:16 +0330

    nike landing page

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ReviewsCard from "./ReviewsCard";
+
+export {
+    PopularProductCard,
+    Button,
+    Nav,
+    ShoeCard,
+    ReviewsCard
+}
